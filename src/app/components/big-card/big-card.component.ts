a3c51abf8fea60a570c7fc6c7bbccac35e23b493
Sol Morcillo <112419272+QSoll@users.noreply.github.com>
2022-12-13 22:54:18 -0300

    Add files via upload

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,18 +6,19 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  
+  @Input()
+  cardTitle:string = ""
 
   @Input()
-  photoCover:string =""
-  @Input()
-  cardTitle:string= ""
-  @Input()
-  cardDescription:string =""
-  @Input()
-  Id:string="0"
+  cardDescripton:string = ""
 
-  constructor() { }
+  @Input()
+  Id:string = '0' 
 
+  constructor(){}
   ngOnInit(): void {
   }
 
