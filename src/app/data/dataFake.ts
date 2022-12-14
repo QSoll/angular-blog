7b737176d6342bde21b0fca1abef0a9b0e4dbea6
Sol Morcillo <112419272+QSoll@users.noreply.github.com>
2022-12-13 22:56:53 -0300

    Add files via upload

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
-  {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
-  },
-  {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    {
+            "id":"1",
+            "title": "Sol Morcillo",
+            "desdription": "Webdeveloper e designer de moda. Para melhorar as apresentações dos desenhos de modelos de roupas, em 2001 fiz um curso de webdesigner e essa junção foi excepcionalmente importante para a minha carreira como estilista. Atualmente em fase de atualização para migrar definitivamente para a área de TI, pois amo <codar>. ",
+            "photoCover":"https://solmorcillo.com.br/assets/images/banner1a_780_500.jpg"
+    },
+    {
+        "id":"2",
+        "title": "MoModIn",
+        "desdription": "Moda e modelagem industrial. Cursos: Desenho de moda, modelagem industrial 2D, modelagem industrial 3D, graduação, desenho de moda, desenho técnico, criaçãod e estampas, transferência de pences, interpretação de modelos, risco, corte e costura. Réguas: 80 cm, boneca vazada e gancho.",
+        "photoCover":"https://solmorcillo.com.br/assets/images/propaganda1.jpg"
+    },
+    {
+        "id":"3",
+        "title": "IOUBI",
+        "desdription": "ERoupas fabricadas com poliamida 6.6 - Emana - tecido tecnológico com vários benefícios para o corpo. Regula a temperatura corporal, ajuda a reduzir medidas, previne a fadiga muscular, ajuda na recuperação da fadiga muscular, melhora o aspecto da pele. As roupas fabricadas com poliamida EMANA devem ser vestidas meia hora antes da ativida física e para pobter os benefícios é necessário usar por 8 horas diárias.",
+        "photoCover":"https://solmorcillo.com.br/assets/images/propaganda2.jpg"
+},
+{
+    "id":"4",
+    "title": "Minhas composições",
+    "desdription": "Em momentos de lazer, a música é um dos meus passatempos favoritos e desde 11 anos escrevo poesias e muitas composições musicais.",
+    "photoCover":"https://solmorcillo.com.br/assets/images/minhasComposicoes1.fw.png"
+}
 ]
