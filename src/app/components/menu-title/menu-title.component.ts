3ff672f5250591f194088e4bb668c50287da9ac1
Sol Morcillo <112419272+QSoll@users.noreply.github.com>
2022-12-13 22:55:47 -0300

    Add files via upload

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: [
-  './menu-title.component.css',
-  './menu-title.responsive.component.css'
+    './menu-title.component.css',
+    './menu-title.responsive.component.css'
   ]
 })
-export class MenuTitleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class MenuTitleComponent {
 
 }
